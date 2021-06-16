35f5a8aeef1a77ce8e74936fc1edf8bc01573de8
Bertus1998 <62727557+Bertus1998@users.noreply.github.com>
2021-06-17 00:08:51 +0200

    Bertus981
    Added wolves and terrain

---
+++ VE/Assets/WaterShader/RefProbePosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefProbePosition : MonoBehaviour
+{
+    public Transform refProbeTransform;
+    public Transform waterTransform;
+
+    void Update()
+    {
+        refProbeTransform.position = new Vector3(
+            this.transform.position.x,
+            this.transform.position.y - (2 * (this.transform.position.y - waterTransform.position.y)),
+            this.transform.position.z
+            );
+    }
+}
